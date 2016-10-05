54fa95ef3ef455c2356b311493e7d45fbbb15271
Josh Newton <josh.newton@careerbuilder.com>
2016-10-05 13:18:25 -0400

    adding working days pr is open and adjusting the conditions controlling urgency

--- src/process/index.js
+++ src/process/index.js
@@ -20,6 +20,7 @@ function mapPRs(repo) {
     PR.owner = PR.owner.login;
     PR.link = PR.url;
     PR.timeOpen = (new Date() - new Date(PR.created)) / oneDay;
+    PR.workingDays = getWorkingDaysSince(PR.created);
     PR.timeSinceLastModified = (new Date() - new Date(PR.updated)) / oneDay;
 
     PR.assignees = _.map(PR.assignees, (assignee) => assignee.login);
@@ -31,6 +32,25 @@ function mapPRs(repo) {
   });
 }
 
+function getWorkingDaysSince(date) {
+  const start = new Date(date);
+  const end = new Date();
+
+  let days = 0;
+  let currentDay = start;
+
+  while(currentDay < end) {
+    console.log(currentDay, currentDay.getDay())
+    currentDay = new Date(currentDay);
+    if(currentDay.getDay() != 0 && currentDay.getDay() != 6) {
+      days++;
+    }
+    currentDay = new Date(currentDay.getTime() + oneDay);
+  }
+
+  return days;
+}
+
 function getLevel(PR) {
   PR.comments = _.filter(PR.comments, (comment) => {
     if(comment.user.login !== PR.owner) {
@@ -39,10 +59,10 @@ function getLevel(PR) {
   });
   let hasLabels = PR.labels.length > 0;
   let noComments = PR.comments.length === 0;
-  if((PR.assignees.length === 0 || PR.timeOpen >= 4) && !hasLabels) {
+  if((PR.assignees.length === 0 || PR.workingDays >= 4) && !hasLabels) {
     return 10; //red: no assignees or stale
   }
-  if((PR.timeOpen > 2 || (PR.timeOpen > 1 && noComments)) && !hasLabels) {
+  if((PR.workingDays >= 2 || (PR.workingDays > 1 && noComments)) && !hasLabels) {
     return 5; //yellow going stale (outstanding > 2 days or no comments from assignees yet && older than 16 hours)
   }
   return 0; //green
